c226fca3c7a921aa68d47aa8207444a154c94c2d
CristianMld <hkrcrm@gmail.com>
2022-10-13 20:02:24 -0300

    third

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const cart = document.querySelector('.cart');
 const addButton = document.querySelectorAll('.add-btn');
+const total = document.querySelector('.total'); 
 
 for (let i = 0; i < addButton.length; i++) {
   addButton[i].addEventListener('click', addToCart);
 };
 
-
+let totalPrice= 0;
 function addToCart(e) {
   let button = e.target;
   let item = button.parentElement.parentElement.parentElement;
   let img = item.querySelector('.photo').src;
   let name = item.querySelector('.name').innerText;
-  let price = item.querySelector('.price').innerText;
+  let price = parseInt(item.querySelector('.price').innerText.replace('$', ''));
+  totalPrice = totalPrice + price;
+  console.log(totalPrice);
   newItemforCart(name, price, img);
 }
 
@@ -22,13 +25,14 @@ function newItemforCart(title, price, img) {
     <img class="img-on-cart" src="${img}" alt="">
     <h3>${title}</h3>
     <h3 class="price">${price}</h3>
-    <input class="quantity" type="number" min="1" max="10">
+    <input class="quantity" type="number" value="1" min="1" max="10">
     <button class="remove">Remove</button>
   </div>`;
   newItem.innerHTML = newItemContent;
   cart.append(newItem); 
   remove();
-}
+  total.innerHTML = `$${totalPrice}`;
+};
 
 function remove() {
   const removeButton = cart.querySelectorAll('.remove');
@@ -39,8 +43,8 @@ function remove() {
       buttonClicked.parentElement.remove();
     });
   };
-}
+};
 
-function updateTotal() {
-  
-}
+cart.addEventListener('click', (e) => {
+  console.log(e.target);
+})
